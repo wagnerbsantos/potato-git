2b10bb1686b8e6d5b21b41867d5a2eebd5882929
Wagner Braga dos Santos <wag.097@gmail.com>
2020-02-20 13:39:44 -0300

    asdf

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
-const PORT = process.env.PORT || 5000;
-var express = require("express");
-var app = express();
-
-app.get("/", function(req, res) {
-  res.send("Hello World!");
-});
-
-app.listen(PORT, function() {
-  console.log("Example app listening on port!" + PORT);
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
